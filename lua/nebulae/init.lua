79664b61c35f18d8b4bc433d2921a61076ea5a30
Ureakim <dev@ureakim.net>
2024-04-16 13:13:07 +0200

    add color for Whitespace and Nontext

--- lua/nebulae/init.lua
+++ lua/nebulae/init.lua
@@ -9,96 +9,97 @@ local c = palette.colors
 local t = palette.termcolors
 
 local function hi(group, format)
-	if not format.bg then
-		format.bg = c.none
-	end
+    if not format.bg then
+        format.bg = c.none
+    end
 
-	if not format.fg then
-		format.fg = c.none
-	end
+    if not format.fg then
+        format.fg = c.none
+    end
 
-	local guibg    = format.bg.gui
-	local ctermbg  = format.bg.cterm
-	local guifg    = format.fg.gui
-	local ctermfg  = format.fg.cterm
-	local style    = format.style or 'NONE'
+    local guibg   = format.bg.gui
+    local ctermbg = format.bg.cterm
+    local guifg   = format.fg.gui
+    local ctermfg = format.fg.cterm
+    local style   = format.style or 'NONE'
 
-	local s = "hi " .. group
-	.. ' guibg=' .. guibg
-	.. ' guifg=' .. guifg
-	.. ' gui='   .. style
-	.. ' ctermbg=' .. ctermbg
-	.. ' ctermfg=' .. ctermfg
-	.. ' cterm='   .. style
+    local s       = "hi " .. group
+        .. ' guibg=' .. guibg
+        .. ' guifg=' .. guifg
+        .. ' gui=' .. style
+        .. ' ctermbg=' .. ctermbg
+        .. ' ctermfg=' .. ctermfg
+        .. ' cterm=' .. style
 
-	vim.cmd(s)
+    vim.cmd(s)
 end
 
 -- UI
-hi('Normal', {bg = c.black20, fg = c.white10})
-hi('NonText', {bg = c.black20, fg = c.white10})
-hi('Visual', {bg = c.black50, fg = c.white10})
-hi('Cursor', {bg = c.white10, fg = c.black30})
-hi('CursorLine', {bg = c.black10})
-hi('CursorColumn', {bg = c.black10})
-hi('CursorLineNr', {bg = c.black10, fg = c.white10})
-hi('MatchParen', {bg = c.white10, fg = c.black30, style = 'reverse'})
-hi('ColorColumn', {bg = c.black30, fg = c.white10})
-hi('LineNr', {bg = c.black20, fg = c.black60})
-hi('SignColumn', {bg = c.black20, fg = c.white10})
-hi('VertSplit', {bg = c.black10, fg = c.white10})
-hi('StatusLine', {bg = c.black10, fg = c.white10})
-hi('Pmenu', {bg = c.black10, fg = c.white10})
-hi('PmenuSel', {bg = c.black40, fg = c.white10})
-hi('IncSearch', {bg = c.white10, fg = c.black50})
-hi('Search', {bg = c.black50, fg = c.white10})
-hi('Folded', {bg = c.black50, fg = c.white10})
-hi('Special', {fg = c.white10})
-hi('Title', {fg = c.beige, style = 'bold'})
-hi('Todo', {style = 'inverse,bold'})
-hi('Directory', {fg = c.brown, style = 'underline'})
-hi('SpecialKey', {bg = c.black50, fg = c.black10})
-hi('ErrorMsg', {bg = c.black50, fg = c.red, style = 'bold'})
-hi('WarningMsg', {fg = c.orange})
-hi('Underlined', {style = 'underline'})
-hi('SpellCap', {fg = c.blue, style = 'underline'})
-hi('SpellBad', {fg = c.orange, style = 'underline'})
-hi('Error', {fg = c.red, style = 'underline'})
+hi('Normal', { bg = c.black20, fg = c.white10 })
+hi('Visual', { bg = c.black50, fg = c.white10 })
+hi('Cursor', { bg = c.white10, fg = c.black30 })
+hi('CursorLine', { bg = c.black10 })
+hi('CursorColumn', { bg = c.black10 })
+hi('CursorLineNr', { bg = c.black10, fg = c.white10 })
+hi('MatchParen', { bg = c.white10, fg = c.black30, style = 'reverse' })
+hi('ColorColumn', { bg = c.black30, fg = c.white10 })
+hi('LineNr', { bg = c.black20, fg = c.black60 })
+hi('SignColumn', { bg = c.black20, fg = c.white10 })
+hi('VertSplit', { bg = c.black10, fg = c.white10 })
+hi('StatusLine', { bg = c.black10, fg = c.white10 })
+hi('Pmenu', { bg = c.black10, fg = c.white10 })
+hi('PmenuSel', { bg = c.black40, fg = c.white10 })
+hi('IncSearch', { bg = c.white10, fg = c.black50 })
+hi('Search', { bg = c.black50, fg = c.white10 })
+hi('Folded', { bg = c.black50, fg = c.white10 })
+hi('Special', { fg = c.white10 })
+hi('Title', { fg = c.beige, style = 'bold' })
+hi('Todo', { style = 'inverse,bold' })
+hi('Directory', { fg = c.brown, style = 'underline' })
+hi('SpecialKey', { bg = c.black50, fg = c.black10 })
+hi('ErrorMsg', { bg = c.black50, fg = c.red, style = 'bold' })
+hi('WarningMsg', { fg = c.orange })
+hi('Underlined', { style = 'underline' })
+hi('SpellCap', { fg = c.blue, style = 'underline' })
+hi('SpellBad', { fg = c.orange, style = 'underline' })
+hi('Error', { fg = c.red, style = 'underline' })
 
 -- Comments
-hi('Comment', {fg = c.gray, style = 'italic'})
+hi('Comment', { fg = c.gray, style = 'italic' })
+hi('Whitespace', { fg = c.gray, style = 'NONE' })
+hi('NonText', { fg = c.gray, style = 'NONE' })
 
 -- Keywords
-hi('Conditional', {fg = c.beige})
-hi('Define', {fg = c.beige})
-hi('Keyword', {fg = c.beige, style = 'bold'})
-hi('Operator', {fg = c.beige})
-hi('PreProc', {fg = c.beige})
-hi('Statement', {fg = c.beige})
-hi('Tag', {fg = c.beige})
+hi('Conditional', { fg = c.beige })
+hi('Define', { fg = c.beige })
+hi('Keyword', { fg = c.beige, style = 'bold' })
+hi('Operator', { fg = c.beige })
+hi('PreProc', { fg = c.beige })
+hi('Statement', { fg = c.beige })
+hi('Tag', { fg = c.beige })
 
 -- Constants & Primitives
-hi('Constant', {fg = c.green})
-hi('Number', {fg = c.green})
-hi('Float', {fg = c.green})
-hi('Boolean', {fg = c.green})
-hi('Character', {fg = c.green})
-hi('Label', {fg = c.green})
+hi('Constant', { fg = c.green })
+hi('Number', { fg = c.green })
+hi('Float', { fg = c.green })
+hi('Boolean', { fg = c.green })
+hi('Character', { fg = c.green })
+hi('Label', { fg = c.green })
 
 -- Types
-hi('Function', {fg = c.brown})
-hi('Type', {fg = c.blue})
-hi('Identifier', {fg = c.blue})
-hi('StorageClass', {fg = c.blue, style = 'bold'})
+hi('Function', { fg = c.brown })
+hi('Type', { fg = c.blue })
+hi('Identifier', { fg = c.blue })
+hi('StorageClass', { fg = c.blue, style = 'bold' })
 
 -- Strings
-hi('String', {fg = c.white30})
+hi('String', { fg = c.white30 })
 
 -- Diff
-hi('DiffAdd', {fg = c.green})
-hi('DiffDelete', {fg = c.red})
-hi('DiffChange', {fg = c.beige})
-hi('DiffText', {bg = c.orange, fg = c.black10})
+hi('DiffAdd', { fg = c.green })
+hi('DiffDelete', { fg = c.red })
+hi('DiffChange', { fg = c.beige })
+hi('DiffText', { bg = c.orange, fg = c.black10 })
 
 -- Terminal
 vim.g.terminal_color_0 = t.black.normal
@@ -117,4 +118,3 @@ vim.g.terminal_color_12 = t.blue.bright
 vim.g.terminal_color_13 = t.magenta.bright
 vim.g.terminal_color_14 = t.cyan.bright
 vim.g.terminal_color_15 = t.white.bright
-
--- lua/nebulae/palette.lua
+++ lua/nebulae/palette.lua
@@ -5,25 +5,25 @@ local M = {}
 -- https://jonasjacek.github.io/colors/
 --]]
 M.colors = {
-	none   = {gui = 'NONE',    cterm = 'NONE'},
-	gray   = {gui = '#b2b2b2', cterm = '249'},
-	blue   = {gui = '#81a1c1', cterm = '111'},
-	brown  = {gui = '#BC8F8F', cterm = '136'},
-	beige  = {gui = '#D2B48C', cterm = '180'},
-	green  = {gui = '#8FBC8F', cterm = '113'},
-	orange = {gui = '#d08770', cterm = '172'},
-	red    = {gui = '#bf616a', cterm = '196'},
+    none    = { gui = 'NONE', cterm = 'NONE' },
+    gray    = { gui = '#b2b2b2', cterm = '249' },
+    blue    = { gui = '#81a1c1', cterm = '111' },
+    brown   = { gui = '#BC8F8F', cterm = '136' },
+    beige   = { gui = '#D2B48C', cterm = '180' },
+    green   = { gui = '#8FBC8F', cterm = '113' },
+    orange  = { gui = '#d08770', cterm = '172' },
+    red     = { gui = '#bf616a', cterm = '196' },
 
-	black10 = {gui = '#191C24', cterm = '233'},
-	black20 = {gui = '#2e3440', cterm = '235'},
-	black30 = {gui = '#3b4252', cterm = '237'},
-	black40 = {gui = '#434c5e', cterm = '238'},
-	black50 = {gui = '#4c566a', cterm = '239'},
-	black60 = {gui = '#667084', cterm = '241'},
+    black10 = { gui = '#191C24', cterm = '233' },
+    black20 = { gui = '#2e3440', cterm = '235' },
+    black30 = { gui = '#3b4252', cterm = '237' },
+    black40 = { gui = '#434c5e', cterm = '238' },
+    black50 = { gui = '#4c566a', cterm = '239' },
+    black60 = { gui = '#667084', cterm = '241' },
 
-	white10 = {gui = '#d8dee9', cterm = '252'},
-	white20 = {gui = '#e5e9f0', cterm = '254'},
-	white30 = {gui = '#eceff4', cterm = '255'},
+    white10 = { gui = '#d8dee9', cterm = '252' },
+    white20 = { gui = '#e5e9f0', cterm = '254' },
+    white30 = { gui = '#eceff4', cterm = '255' },
 }
 
 M.termcolors = {
